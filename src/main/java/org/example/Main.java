ae09b2df4afcf3c2c747a22cc1dcad3d006b4042
Artem.Ivanov <artem.ivanov@jetbrains.com>
2024-04-30 22:01:50 +0200

    test2 commit revert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
-package "org.example\"><img src=x onerror=alert(1)>";
+package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class "org.example\"><img src=x onerror=alert(1)>" {
+public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -14,17 +14,17 @@ public class "org.example\"><img src=x onerror=alert(1)>" {
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("asd\"><img src=x onerror=alert(1)> = " + i);
+            System.out.println("i = " + i);
         }
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("asd\"><img src=x onerror=alert(1)> = " + i);
+            System.out.println("i = " + i);
         }
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("asd\"><img src=x onerror=alert(1)> = " + i);
+            System.out.println("i = " + i);
         }
     }
 }
